5c79c2f95c62ba64c357154233ad093f622ea410
Morgan Leborgne <morganleborgne@gmail.com>
2010-06-13 20:36:43 +0200

    Fix some warnings - no functional changes

--- lib/database/include/Inventor/SbRotationd.h
+++ lib/database/include/Inventor/SbRotationd.h
@@ -49,7 +49,7 @@
  |	such as vectors, coordinates, etc..
  |
  |   Classes:
- |	SbRotation
+ |	SbRotationd
  |
  |   Author(s)		: Paul S. Strauss, Nick Thompson, 
  |			  David Mott, Alain Dumesny
@@ -58,66 +58,66 @@
  _______________________________________________________________________
  */
 
-#ifndef _SB_ROTATION_
-#define _SB_ROTATION_
+#ifndef _SB_ROTATIOND_
+#define _SB_ROTATIOND_
 
 #include <Inventor/SbVec4f.h>
 
 class SbMatrix;
 class SbMatrixd;
-class SbVec3f;
+class SbVec3d;
 
 /// Class for representing a rotation.
 /// \ingroup Basics
 /// Object that stores a rotation. There are several ways to specify a
-/// rotation: quaternion (4 floats), 4x4 rotation matrix, or axis and angle.
+/// rotation: quaternion (4 doubles), 4x4 rotation matrix, or axis and angle.
 /// All angles are in radians and all rotations are right-handed.
 /// \par Notes:
 /// Rotations are stored internally as quaternions.
-/// \sa SbVec3f, SbMatrix
-class INVENTOR_API SbRotation {
+/// \sa SbRotation, SbVec3f, SbVec4f, SbMatrix
+class INVENTOR_API SbRotationd {
 public:
 
     /// Default constructor
-    SbRotation() {
-        setValue(0.0f, 0.0f, 0.0f, 1.0f);
+    SbRotationd() {
+        setValue(0.0, 0.0, 0.0, 1.0);
     }
 
     /// Constructor given a quaternion as an array of 4 components
-    SbRotation(const float v[4]) {
+    SbRotationd(const double v[4]) {
         setValue(v);
     }
 
     /// Constructor given 4 individual components of a quaternion
-    SbRotation(float q0, float q1, float q2, float q3) {
+    SbRotationd(double q0, double q1, double q2, double q3) {
         setValue(q0, q1, q2, q3);
     }
 
     /// Constructor given a rotation matrix
-    SbRotation(const SbMatrix &m) {
+    SbRotationd(const SbMatrixd &m) {
         setValue(m);
     }
 
     /// Constructor given 3D rotation axis vector and angle in radians
-    SbRotation(const SbVec3f &axis, float radians) {
+    SbRotationd(const SbVec3d &axis, double radians) {
         setValue(axis, radians);
     }
 
     /// Constructor for rotation that rotates one direction vector to another
-    SbRotation(const SbVec3f &rotateFrom, const SbVec3f &rotateTo) {
+    SbRotationd(const SbVec3d &rotateFrom, const SbVec3d &rotateTo) {
         setValue(rotateFrom, rotateTo);
     }
 
     /// Returns pointer to array of 4 components defining quaternion
-    const float * getValue() const {
+    const double * getValue() const {
         return (quat);
     }
 
     /// Returns 4 individual components of rotation quaternion
-    void  getValue(float &q0, float &q1, float &q2, float &q3) const;
+    void  getValue(double &q0, double &q1, double &q2, double &q3) const;
 
     /// Returns corresponding 3D rotation axis vector and angle in radians
-    void  getValue(SbVec3f &axis, float &radians) const;
+    void  getValue(SbVec3d &axis, double &radians) const;
 
     /// Returns corresponding 4x4 rotation matrix
     void  getValue(SbMatrix &matrix) const;
@@ -126,79 +126,79 @@ public:
     void  getValue(SbMatrixd &matrix) const;
 
     /// Changes a rotation to be its inverse
-    SbRotation & invert();
+    SbRotationd & invert();
 
     /// Returns the inverse of a rotation
-    SbRotation  inverse() const {
-        SbRotation q = *this;
+    SbRotationd  inverse() const {
+        SbRotationd q = *this;
         return q.invert();
     }
 
     /// Sets value of rotation from array of 4 components of a quaternion
-    SbRotation & setValue(const float q[4]);
+    SbRotationd & setValue(const double q[4]);
 
     /// Sets value of rotation from 4 individual components of a quaternion
-    SbRotation & setValue(float q0, float q1, float q2, float q3);
+    SbRotationd & setValue(double q0, double q1, double q2, double q3);
 
     /// Sets value of rotation from a rotation matrix
     /// I don't know what will happen if you call this with something
     /// that isn't a rotation.
-    SbRotation & setValue(const SbMatrix &m);
+    SbRotationd & setValue(const SbMatrix &m);
 
     /// Sets value of rotation from a rotation matrix
-    SbRotation & setValue(const SbMatrixd &m);
+    SbRotationd & setValue(const SbMatrixd &m);
 
     /// Sets value of vector from 3D rotation axis vector and angle in radians
-    SbRotation & setValue(const SbVec3f &axis, float radians);
+    SbRotationd & setValue(const SbVec3d &axis, double radians);
 
     /// Sets rotation to rotate one direction vector to another
-    SbRotation & setValue(const SbVec3f &rotateFrom,
-                          const SbVec3f &rotateTo);
+    SbRotationd & setValue(const SbVec3d &rotateFrom,
+                          const SbVec3d &rotateTo);
 
     /// Multiplies by another rotation; results in product of rotations
-    SbRotation &  operator *=(const SbRotation &q);
+    SbRotationd &  operator *=(const SbRotationd &q);
 
     /// Equality comparison operator
-    friend INVENTOR_API int operator ==(const SbRotation &q1, const SbRotation &q2);
+    friend INVENTOR_API int operator ==(const SbRotationd &q1, const SbRotationd &q2);
 
     /// Inequality comparison operator
-    friend INVENTOR_API int operator !=(const SbRotation &q1, const SbRotation &q2) {
+    friend INVENTOR_API int operator !=(const SbRotationd &q1, const SbRotationd &q2) {
         return !(q1 == q2);
     }
 
     /// Equality comparison within given tolerance - the square of the
     /// length of the maximum distance between the two quaternion vectors
-    SbBool  equals(const SbRotation &r, float tolerance) const;
+    SbBool  equals(const SbRotationd &r, double tolerance) const;
 
     /// Multiplication of two rotations; results in product of rotations
-    friend INVENTOR_API SbRotation operator *(const SbRotation &q1, const SbRotation &q2);
+    friend INVENTOR_API SbRotationd operator *(const SbRotationd &q1, const SbRotationd &q2);
 
     /// Puts the given vector through this rotation
     /// (Multiplies the given vector by the matrix of this rotation),.
-    void multVec(const SbVec3f &src, SbVec3f &dst) const;
+    void multVec(const SbVec3d &src, SbVec3d &dst) const;
 
     /// Keep the axis the same. Multiply the angle of rotation by
     /// the amount 'scaleFactor'
-    void scaleAngle( float scaleFactor );
+    void scaleAngle( double scaleFactor );
 
     /// Spherical linear interpolation: as t goes from 0 to 1, returned
     /// value goes from rot0 to rot1
-    static SbRotation slerp(const SbRotation &rot0,
-                            const SbRotation &rot1, float t);
+    static SbRotationd slerp(const SbRotationd &rot0,
+                            const SbRotationd &rot1, double t);
 
     /// Returns a null rotation.
-    static SbRotation identity() {
-        return SbRotation(0.0, 0.0, 0.0, 1.0);
+    static SbRotationd identity() {
+        return SbRotationd(0.0, 0.0, 0.0, 1.0);
     }
 
 private:
-    float quat[4]; // Storage for quaternion components
+    double quat[4]; // Storage for quaternion components
 
     // Returns the norm (square of the 4D length) of a rotation's quaterion
-    float norm() const;
+    double norm() const;
 
     // Normalizes a rotation quaternion to unit 4D length
     void normalize();
 };
 
-#endif /* _SB_ROTATION_ */
+#endif /* _SB_ROTATIOND_ */
