0210ac8a51d71ad0c77bbccbb2b41db992f68a23
Morgan Leborgne <leborgne@zib.de>
2012-05-30 09:47:00 +0200

    libInventor: SbString unicode support.
    SbString store now UTF-8 internally.

--- ivtest/testString.cpp
+++ ivtest/testString.cpp
@@ -3,6 +3,21 @@
 #include <Inventor/SbString.h>
 
 #include <QString>
+#include <math.h>
+
+TEST(SbString, Constructors) {
+    SbString string1("an ascii string");
+
+    EXPECT_TRUE(string1 == "an ascii string");
+
+    SbString string2("an ascii string", 3, 7);
+
+    EXPECT_TRUE(string2 == "ascii");
+
+    SbString string3(123456789);
+
+    EXPECT_TRUE(string3 == "123456789");
+}
 
 TEST(SbString, Empty) {
 
@@ -62,14 +77,98 @@ TEST(SbString, Operators) {
     sbstring = NULL;
 
     EXPECT_TRUE(sbstring.isEmpty());
+
+    SbString stringA = "an ascii string";
+    SbString stringB = "an ascii string";
+
+    EXPECT_TRUE(stringA == stringB);
+    EXPECT_TRUE(stringA == "an ascii string");
+
+    stringB.makeEmpty();
+    stringB += stringA;
+
+    EXPECT_TRUE(stringA == stringB);
+
+    stringB.makeEmpty();
+    stringB += "an ascii string";
+
+    EXPECT_TRUE(stringA == stringB);
 }
 
-TEST(SbString, Unicode) {
+TEST(SbString, UTF8) {
 
-    QString German = QString::fromUtf8("Ä ä Ü ü ß");
-    QString Russian = QString::fromUtf8("Я Б Г Д Ж Й");
-    QString Polish = QString::fromUtf8("Ł Ą Ż Ę Ć Ń Ś Ź");
-    QString Japanese = QString::fromUtf8("ｱｲｳｴｵｶｷｸｹｺｻｼｽｾｿﾀﾁﾂﾃ");
+    //Swedish "Kluft skrams infor pa federal electoral groe"
+    SbString string1 = SbString::fromUtf8("Kl\303\274ft skr\303\244ms inf\303\266r p\303\245 f\303\251d\303\251ral \303\251lectoral gro\303\237e");
+    SbString string2 = SbString::fromUtf8("Kl\303\274ft skr\303\244ms inf\303\266r ");
+    SbString string3 = SbString::fromUtf8("p\303\245 f\303\251d\303\251ral \303\251lectoral gro\303\237e");
 
-    //printf("utf8: %s\n", Japanese.toUtf8().constData());
+    EXPECT_EQ(string1.getLength(), 45);
+    EXPECT_EQ(string2.getLength(), 19);
+    EXPECT_EQ(string3.getLength(), 26);
+
+    SbString string4 = string2 + string3;
+    EXPECT_EQ(string4.getLength(), 45);
+
+    EXPECT_TRUE(string1 == string4);
+}
+
+
+int Dec2Oct(int n)
+{
+    int i=0,r,b=0;
+    while(n!=0)
+    {
+        r=n%8;
+        b=b+pow(10.0f,i)*r;
+        n=n/8;
+        i++;
+    }
+    return b;
+}
+
+void printstring(const SbString & str) {
+    const unsigned char *data = (unsigned char*)str.getString();
+    for (int i=0; i<strlen(str.getString()); i++) {
+        if (data[i] < 0x80) {
+            printf("%c", data[i]);
+        } else {
+            printf("\\%d", Dec2Oct(data[i]));
+        }
+    }
+    printf("\n");
+}
+
+TEST(SbString, WideChar) {
+    SbString string1 = SbString::fromWideChar(L"Hello World");
+    SbString string2 = SbString::fromWideChar(L"Hello ");
+    SbString string3 = SbString::fromWideChar(L"World");
+
+    EXPECT_EQ(string1.getLength(), 11);
+    EXPECT_EQ(string2.getLength(), 6);
+    EXPECT_EQ(string3.getLength(), 5);
+
+    SbString string4 = string2 + string3;
+    EXPECT_EQ(string4.getLength(), 11);
+
+    EXPECT_TRUE(string1 == string4);
+
+    std::wstring stdstring = L"Hello World";
+    EXPECT_EQ(string1.getLength(), stdstring.length());
+    EXPECT_TRUE(stdstring == string1.toStdWString());
+
+    SbString string5 = SbString::fromUtf8("Kl\303\274ft skr\303\244ms inf\303\266r p\303\245 f\303\251d\303\251ral \303\251lectoral gro\303\237e");
+    stdstring = string5.toStdWString();
+    SbString string6 = SbString::fromWideChar(stdstring.data());
+    EXPECT_TRUE(string5 == string6);
+
+    //std::wcout << stdstring << std::endl;
+
+    SbString string7 = SbString::fromUtf8("Hello World");
+    EXPECT_TRUE(string1 == string7);
+
+    // Japaneese: the quick brown fox jumped over the lazy dog
+    SbString string8 = SbString::fromUtf8("\351\200\237\343\201\204\350\214\266\350\211\262\343\201\256\343\202\255\343\203\204\343\203\215\343\201\257\346\200\240\346\203\260\343\201\252\347\212\254\345\242\227\344\273\245\344\270\212");
+    EXPECT_TRUE(string8.getLength() == 16);
+    stdstring = string8.toStdWString();
+    EXPECT_TRUE(stdstring.length() == 16);
 }
