457f8914d8fbf25f8fd2bed8032238d826e75a8b
Morgan Leborgne <leborgne@zib.de>
2012-06-21 11:42:32 +0200

    libInventor: SbFile interface adapted to SoInput/output requirements.

--- libInventor/database/include/Inventor/SbFile.h
+++ libInventor/database/include/Inventor/SbFile.h
@@ -6,23 +6,29 @@
 
 class INVENTOR_API SbFile {
 public:
-    /// Constructs a new file object to represent the file with the given fileName.
-    SbFile(const SbString & fileName);
+    /// Constructs a new file object.
+    SbFile();
     
+    /// Constructs a new file object.
+    SbFile(FILE *fp);
+
     /// Destroys the file object, closing it if necessary.
     ~SbFile();
 
-    /// Returns the name set to the SbFile constructor.
-    const SbString & fileName() const{ return mFileName; }
-
     /// Opens the device with the specified mode. Returns true if successful; otherwise returns false.
-    bool open( const SbString & mode );
+    bool open(const SbString & fileName, const SbString & mode );
     
     /// Returns true if the file is open; otherwise returns false. 
     bool isOpen() const;
 
     /// Closes the file.
     void close();
+
+    /// Sets file pointer to read from.
+    void setFilePointer(FILE *fp);
+
+    /// Returns the file pointer.
+    FILE *getFilePointer() const;
 
     /// Returns true if the file exists; otherwise returns false.
     bool exists() const;
@@ -33,35 +39,38 @@ public:
     /// Sets the current position to pos, returning true on success, or false if an error occurred.
     bool seek(int64_t offset);
 
-    /// Returns the size of the file.
-    int64_t size() const;
+    /// Returns true if the end of the file has been reached; otherwise returns false.
+    bool eof() const;
 
     /// Returns the number of full items actually read, which may be less than count if an error occurs or if the end of the file is encountered before reaching count.
-    size_t read(char * data, size_t count);
+    size_t read(void * data, size_t size, size_t count);
 
     /// Writes up to count items.
-    size_t write(const char * data, size_t count);
+    size_t write(const void * data, size_t size, size_t count);
 
-    /// Returns the base name of the file without the path.
-    SbString baseName() const;
+    /// Flushes any buffered data to the file. Returns true if successful; otherwise returns false.
+    bool flush();
 
-    /// Returns the complete extension of the file.
-    SbString extension() const;
+    /// Returns the size of the file.
+    static int64_t size(const SbString & fileName);
 
     /// Returns true if the file specified by fileName exists; otherwise returns false.
-    static bool exists( const SbString & fileName );
+    static bool exists(const SbString & fileName);
 
     /// Static version of baseName().
     static SbString baseName(const SbString & filename);
 
     /// Static version of extension().
     static SbString extension(const SbString & filename);
+
+    /// Gets directory part of file name including the trailing @c /.
+    static SbString dirName(const SbString & filename);
 private:
     // Disables the use of copy constructors and assignment operators
     SbFile(const SbFile &);
     SbFile &operator=(const SbFile &);
 
-    SbString    mFileName;
+    bool        mOwner; // TRUE if SbFile opened file
     FILE *      mFilePointer;
 };
 
