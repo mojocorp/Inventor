d03ce6709517a059ab7f7fa82ed4dbbe10d0c8d0
Morgan Leborgne <leborgne@zib.de>
2012-06-21 11:42:32 +0200

    libInventor: SbFile interface adapted to SoInput/output requirements.

--- libInventor/database/src/sb/SbFile.cpp
+++ libInventor/database/src/sb/SbFile.cpp
@@ -15,11 +15,18 @@
 
 #include <sys/stat.h>
 
-SbFile::SbFile(const SbString & fn)
-: mFileName(fn),
+SbFile::SbFile()
+: mOwner(false),
   mFilePointer(NULL)
 {
 
+}
+
+SbFile::SbFile(FILE *fp)
+: mOwner(false),
+  mFilePointer(NULL)
+{
+    setFilePointer(fp);
 }
 
 SbFile::~SbFile()
@@ -28,14 +35,16 @@ SbFile::~SbFile()
 }
 
 bool 
-SbFile::open( const SbString & mode )
+SbFile::open(const SbString & fileName, const SbString & mode )
 {
-    if (isOpen())
-        return false;
+    close();
+
+    mOwner = true;
+
 #ifdef SB_OS_WIN
-    mFilePointer = _wfopen(mFileName.toStdWString().data(), mode.toStdWString().data());
+    mFilePointer = _wfopen(fileName.toStdWString().data(), mode.toStdWString().data());
 #else
-    mFilePointer = fopen(mFileName.getString(), mode.getString());
+    mFilePointer = fopen(fileName.getString(), mode.getString());
 #endif
     return isOpen();
 }
@@ -50,16 +59,25 @@ void
 SbFile::close()
 {
     if (mFilePointer) {
-        fclose(mFilePointer);
+        if (mOwner)
+            fclose(mFilePointer);
         mFilePointer = NULL;
     }
 }
 
-
-bool
-SbFile::exists() const
+void
+SbFile::setFilePointer(FILE *fp)
 {
-    return SbFile::exists(mFileName);
+    close();
+
+    mFilePointer = fp;
+    mOwner = false;
+}
+
+FILE *
+SbFile::getFilePointer() const
+{
+    return mFilePointer;
 }
 
 int64_t 
@@ -74,18 +92,41 @@ SbFile::seek(int64_t offset)
     return SB_FSEEK(mFilePointer, offset, SEEK_SET) == 0;
 }
 
+bool
+SbFile::eof() const
+{
+    return feof(mFilePointer) != 0 ? true : false;
+}
+
+size_t 
+SbFile::read(void * data, size_t size, size_t count)
+{
+    return fread(data, size, count, mFilePointer);
+}
+
+size_t 
+SbFile::write(const void * data, size_t size, size_t count)
+{
+    return fwrite(data, size, count, mFilePointer);
+}
+
+bool
+SbFile::flush()
+{
+    return (fflush(mFilePointer) == 0);
+}
 
 int64_t
-SbFile::size() const
+SbFile::size(const SbString & fileName)
 {
 #if defined(SB_OS_WIN)
     struct _stati64 st;
-    if (_wstati64(mFileName.toStdWString().data(), &st) == 0) {
+    if (_wstati64(fileName.toStdWString().data(), &st) == 0) {
         return st.st_size;
     }
 #else
     struct stat st;
-    if (stat(mFileName.getString (), &st) == 0) {
+    if (stat(fileName.getString (), &st) == 0) {
         return st.st_size;
     }
 #endif
@@ -93,20 +134,8 @@ SbFile::size() const
     return -1;
 }
 
-size_t 
-SbFile::read(char * data, size_t count)
-{
-    return fread(data, 1, count, mFilePointer);
-}
-
-size_t 
-SbFile::write(const char * data, size_t count)
-{
-    return fwrite(data, 1, count, mFilePointer);
-}
-
 bool
-SbFile::exists( const SbString & fileName )
+SbFile::exists(const SbString & fileName)
 {
 #ifdef SB_OS_WIN
     return (::_waccess(fileName.toStdWString().data(),00)==0) ? true : false;
@@ -116,31 +145,27 @@ SbFile::exists( const SbString & fileName )
 }
 
 SbString
-SbFile::baseName() const
+SbFile::baseName(const SbString & filename)
 {
-    return SbFile::baseName(mFileName);
+    int index = std::max( filename.rfind("/"), filename.rfind("\\"));
+
+    return filename.getSubString(index+1);
 }
 
 SbString
-SbFile::extension() const
+SbFile::extension(const SbString & filename)
 {
-    return SbFile::extension(mFileName);
-}
-
-SbString
-SbFile::baseName(const SbString & string)
-{
-    int index = std::max( string.rfind("/"), string.rfind("\\"));
-
-    return string.getSubString(index+1);
-}
-
-SbString
-SbFile::extension(const SbString & string)
-{
-    int firstDot = string.find(".", baseName(string).getLength());
+    int firstDot = filename.find(".", (int)baseName(filename).getLength());
     if (firstDot == -1)
         return "";
 
-    return string.getSubString(firstDot + 1);
+    return filename.getSubString(firstDot + 1);
+}
+
+SbString
+SbFile::dirName(const SbString & filename)
+{
+    int index = std::max( filename.rfind("/"), filename.rfind("\\"));
+
+    return filename.getSubString(0, index);
 }
