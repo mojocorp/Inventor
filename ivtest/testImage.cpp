6cb76df937ac017aa60068c22b56fb99e5e09b35
Morgan Leborgne <leborgne@zib.de>
2012-12-03 21:22:00 +0100

    ivtest: use md5 for image readers.

--- ivtest/testImage.cpp
+++ ivtest/testImage.cpp
@@ -3,6 +3,8 @@
 
 #include <Inventor/SbImage.h>
 
+#include <QCryptographicHash>
+
 TEST(testImage, jpg) {
     SbString filename = getenv("OIV_HOME") + SbString("/data/textures/brick.jpg");
 
@@ -13,11 +15,9 @@ TEST(testImage, jpg) {
     EXPECT_EQ(128, img.getSize()[1]);
     EXPECT_EQ(SbImage::Format_RGB24, img.getFormat());
 
-    EXPECT_EQ(147, img.getConstBytes()[3*(0+0*128)]);
-    EXPECT_EQ(188, img.getConstBytes()[3*(127+0*128)]);
-    EXPECT_EQ(213, img.getConstBytes()[3*(127+127*128)]);
-    EXPECT_EQ(169, img.getConstBytes()[3*(0+127*128)]);
-    EXPECT_EQ(204, img.getConstBytes()[3*(64+64*128)]);
+    QCryptographicHash hash(QCryptographicHash::Md5);
+    hash.addData((const char*)img.getConstBytes(), 3*128*128);
+    EXPECT_TRUE(hash.result().toHex() == "5e825515437455dc1d38309ec909a9b0");
 }
 
 TEST(testImage, rgb) {
@@ -30,9 +30,7 @@ TEST(testImage, rgb) {
     EXPECT_EQ(128, img.getSize()[1]);
     EXPECT_EQ(SbImage::Format_RGB24, img.getFormat());
 
-    EXPECT_EQ(151, img.getConstBytes()[3*(0+0*128)]);
-    EXPECT_EQ(184, img.getConstBytes()[3*(127+0*128)]);
-    EXPECT_EQ(211, img.getConstBytes()[3*(127+127*128)]);
-    EXPECT_EQ(172, img.getConstBytes()[3*(0+127*128)]);
-    EXPECT_EQ(205, img.getConstBytes()[3*(64+64*128)]);
+    QCryptographicHash hash(QCryptographicHash::Md5);
+    hash.addData((const char*)img.getConstBytes(), 3*128*128);
+    EXPECT_TRUE(hash.result().toHex() == "ebd3a848456dce1df933a30bcd722cec");
 }
