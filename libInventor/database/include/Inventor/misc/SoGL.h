6d1ee31e4c5c38074a0620595284299992152d1c
Morgan Leborgne <leborgne@zib.de>
2013-06-01 12:29:31 +0200

    test

--- libInventor/database/include/Inventor/misc/SoGL.h
+++ libInventor/database/include/Inventor/misc/SoGL.h
@@ -1,9 +1,11 @@
 #ifndef _SO_GL_
 #define _SO_GL_
 
-#include <Inventor/misc/SoGLContext.h>
+//#include <Inventor/misc/SoGLContext.h>
 
-#define glewGetContext() SoGLContext::getCurrentContext()
+//#define glewGetContext() SoGLContext::getCurrentContext()
+
+#include <Inventor/misc/GL/gl.h>
 
 #ifdef SB_OS_MACX
 #   include <OpenGL/glu.h>
